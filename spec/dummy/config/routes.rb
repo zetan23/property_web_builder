6637699165e4b6298706cdad441ef53ad0b693c8
Ed Tewiah <etewiah@hotmail.cim>
2017-03-18 19:13:18 +0100

    Setup CMS

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   mount Pwb::Engine => '/'
+
+  comfy_route :cms_admin, :path => '/cms/admin'
+
+  # Make sure this routeset is defined last
+  comfy_route :cms, :path => '/cms/', :sitemap => false
 end
